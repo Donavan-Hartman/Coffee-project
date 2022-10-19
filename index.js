a60d677f25fb4e9555e7955bb63b25d038f5503d
Donavan-Hartman <104030591+Donavan-Hartman@users.noreply.github.com>
2022-10-19 16:32:41 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+  const mobile_menu = document.querySelector(".mobile-nav");
+  mobile_menu.classList.toggle("is-active");
+}
+
+function alertUser() {
+  alert("thank you");
+}
